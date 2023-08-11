887c21e5e1cc4103d824b81650a4c07652938119
Thomas Hiller <thomas.hiller@gmx.de>
2023-08-11 19:37:34 +0200

    support Windows

--- scaled-ootr.py
+++ scaled-ootr.py
@@ -1,10 +1,15 @@
 import glob
 import json
 import os
+
+# The bin folder has the DLLs
+os.environ['path'] += r';D:\Projects\vips-dev-8.14\bin'
+
 import pyvips
 
 source = './OoT-Reloaded-SoH/OoT Reloaded (SoH)'
 target = './new/'
+scaling_factor = 4
 
 with open(source + "/manifest.json", "r") as manifest_file:
   manifest = json.load(manifest_file)
@@ -13,15 +18,17 @@ png_list = glob.glob('**/*.png', root_dir=source, recursive=True)
 
 for png in png_list:
     image = pyvips.Image.new_from_file(source + "/" + png, access='random')
-    png_noext = os.path.splitext(png)[0]
+    png_new = png.replace("\\","/")
+    png_noext = os.path.splitext(png_new)[0]
+    
     try:
       info = manifest[png_noext]
 
       print(png)
       print(info)
 
-      height_factor = info["textureHeight"]*4 / image.height
-      width_factor = info["textureWidth"]*4 / image.width
+      height_factor = info["textureHeight"]*scaling_factor / image.height
+      width_factor = info["textureWidth"]*scaling_factor / image.width
       image = image.affine((width_factor, 0, 0, height_factor))
       os.makedirs(target + os.path.dirname(png), exist_ok=True)
 
